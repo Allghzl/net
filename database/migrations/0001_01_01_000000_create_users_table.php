9ce5e49eb077f4cc0767640c0b4d9fdd5e5ff819
Daffa Azka <azkadaiki26@gmail.com>
2025-04-18 15:33:24 +0700

    fix minor bug on plan route and create movie factory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            // Sementara dipakai default untuk minimalisir bug (tunggu sampai dashboardnya kelar) - destcode
+            $table->string('name')->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
